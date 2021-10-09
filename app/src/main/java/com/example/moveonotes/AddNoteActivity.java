1361fe2d5971c71e4a6524adf4b230429f241273
Mor Ofir <morofir92@gmail.com>
2021-10-09 20:54:02 +0300

    9/10 commit, AUTH in MVVM done.

--- app/src/main/java/com/example/moveonotes/AddNoteActivity.java
+++ app/src/main/java/com/example/moveonotes/AddNoteActivity.java
@@ -191,7 +191,7 @@ public class AddNoteActivity extends AppCompatActivity {
                 else
                 {
                     takePic();
-                }//
+                }//todo
 
             }
         });
